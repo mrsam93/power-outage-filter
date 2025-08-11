121de1e7222aff90bd92729a6040ae9392df0a61
Saman Ghadri <49528128+mrsam93@users.noreply.github.com>
2025-08-11 16:19:17 +0330

    Update power-outage-filter.php

--- power-outage-filter/power-outage-filter.php
+++ power-outage-filter/power-outage-filter.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: فیلتر محصولات در زمان قطعی برق
- * Plugin URI:  https://saranhosting.com
+ * Plugin URI:  https://github.com/mrsam93/power-outage-filter
  * Description: فقط محصولات انتخاب‌شده را در زمان قطعی برق نمایش می دهد.
  * Version:     1.1.0
  * Author:      SaranHosting (Saman Ghadri)
@@ -30,4 +30,5 @@ require_once plugin_dir_path(__FILE__) . 'includes/taxonomy.php';
 require_once plugin_dir_path(__FILE__) . 'includes/filters.php';
 require_once plugin_dir_path(__FILE__) . 'includes/admin-ui.php';
 require_once plugin_dir_path(__FILE__) . 'includes/settings.php';
+
 
